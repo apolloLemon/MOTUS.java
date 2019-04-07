cd0ce811c8172b52b2bc9b6249f9c4674ea20b26
Matthew Coyle <matthew@coyle.fr>
2019-04-07 15:44:08 +0200

    prototyping for Game Board Line Dictionary Clue

--- source.java
+++ source.java
@@ -1,7 +1,44 @@
-class Board {}
+class Game {
+	Board[] boards;
+	Dictionary dictionary;
+	int score;
 
-class Line {}
+	void GenerateBoards(){return;}
+}
 
-class Game {}
+class Board {
+	boolean played; //flag to see if the board has been played
+	Line[] lines;
+	Dictionary dictionary;
+	Clue banked; //this is to store clues between lines
 
-class Dictionary {}
+	boolean solved(){return true;}
+	boolean lost(){return true;}
+}
+
+class Line {
+	boolean played; //flag to see if the line has been played
+	char[] chars;
+
+	void ColorClues(){return;}
+	void Prefil(){return;}
+	void ReadPlayer(){return;}
+	boolean CheckWord(){return;}
+}
+
+class Dictionary {
+	String[] words;
+	String word;
+
+	boolean wordMatch(String s){return true;}
+	void newWord(){return;}
+	void loadDictionary(){return;}
+	Clue getClues() {return new Clue();}
+}
+
+class Clue {
+	char[] correct; //letter correctly placed
+	char[] inWord; //lerrer in the word
+
+	void Update (Clue new) {return;} //take in new clue info
+}
